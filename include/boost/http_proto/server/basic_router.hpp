05cde01be8bf7cedee796aeb9b0ad87a524f03a0
Mungo Gill <mungo.gill@me.com>
2025-12-08 16:59:05 +0000

    Workaround Apple linker deduplication bug
    Workaround Apple linker deduplication bug

--- include/boost/http_proto/server/basic_router.hpp
+++ include/boost/http_proto/server/basic_router.hpp
@@ -132,7 +132,7 @@ private:
 };
 
 //-----------------------------------------------
- 
+
 //namespace detail {
 
 class any_router;
@@ -1009,6 +1009,11 @@ private:
             }
             catch(E const& ex)
             {
+#ifdef __APPLE__
+                static volatile int c = 0;
+                if(c != 0)
+                    p_.ep_ = nullptr;
+#endif // APPLE
                 set_resume u(p_);
                 // VFALCO What if h throws?
                 auto rv = h(p, ex);
